7ee5af5d5ac26b398ab0fec20241a06a979d59d3
MJ <mj@sweettracker.co.kr>
2020-02-05 19:07:20 +0900

    first commit

---
+++ app/src/main/java/com/example/sweettrackermobiletest/TrackingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sweettrackermobiletest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrackingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tracking)
+    }
+}
---
+++ app/src/main/java/com/example/sweettrackermobiletest/tracking/Model.kt
@@ -0,0 +1,4 @@
+package com.example.sweettrackermobiletest.tracking
+
+class Model {
+}
